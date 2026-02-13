78e02663bd6d3c15c7d7d16000d7d17f0f12f1e5
zackmorgs <zackmorgenthaler@gmail.com>
2026-02-13 09:58:32 -0600

    fixed events

--- page-events.php
+++ page-events.php
@@ -8,7 +8,7 @@
 <main class="main-content">
     <section id="event_services">
         <div class="content">
-            <a href="" class="btn">
+            <a href="https://www.facebook.com/ashbringermusic/events" class="btn">
                 Facebook Events
             </a>
             <br>
@@ -25,11 +25,11 @@
             </a>
         </div>
     </section>
-    <section id="bands_in_town">
-        <div class="content">
+    <!-- <section id="bands_in_town">
+        <div class="content content-white">
             <script charset="utf-8" src="https://widget.bandsintown.com/main.min.js"></script><a class="bit-widget-initializer" data-artist-name="ashbringer" data-display-local-dates="false" data-display-past-dates="false" data-auto-style="false" data-text-color="#000000" data-link-color="#00b4b3" data-background-color="rgba(0,0,0,0)" data-display-limit="15" data-display-start-time="false" data-link-text-color="#FFFFFF" data-display-lineup="false" data-display-play-my-city="true" data-separator-color="rgba(124,124,124,0.25)"></a>
         </div>
-    </section>
+    </section> -->
     <section id="event_content">
         <div class="content">
         <?php
